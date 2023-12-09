422c1aa7975e6b2464a148f1c662e3b79d63a91f
javier.varela <javier.varela@udla.edu.ec>
2023-12-09 15:13:59 -0500

    Add project files.

---
+++ AirsoftAPI/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirsoftAPI.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
